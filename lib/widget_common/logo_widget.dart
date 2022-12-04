27b846db4bb8adcf3ee05844f2b520b5bf7b2e5a
abdalla3yash <abdo3yash@gmail.com>
2022-12-04 02:15:47 -0800

    splash screen

---
+++ lib/widget_common/logo_widget.dart
@@ -0,0 +1,10 @@
+import 'package:mart/consts/consts.dart';
+
+Widget appLogoWidget() {
+  return Image.asset(icAppLogo)
+      .box
+      .white
+      .size(77, 77)
+      .padding(const EdgeInsets.all(8))
+      .make();
+}
