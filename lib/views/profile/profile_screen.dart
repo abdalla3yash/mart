06fc43e2be2282bfbb2753bfb49bd3616296abf9
abdalla3yash <abdo3yash@gmail.com>
2022-12-09 07:12:14 -0800

    finish UI

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -8,65 +8,106 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return bgWidget(
-        child: Scaffold(
-      body: SafeArea(
-          child: Container(
-        padding: const EdgeInsets.all(8),
+      child: Scaffold(
+          body: SafeArea(
         child: Column(
           children: [
-            Align(
-                alignment: Alignment.topRight,
-                child: const Icon(
-                  Icons.edit,
-                  color: whiteColor,
-                ).onTap(() {})),
-            Row(
-              children: [
-                Image.asset(
-                  imgProfile,
-                  width: 100,
-                  fit: BoxFit.cover,
-                ).box.roundedFull.clip(Clip.antiAlias).make(),
-                5.heightBox,
-                Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    "Dummy User".text.fontFamily(semibold).white.make(),
-                    "customer@example.com".text.white.make()
-                  ],
-                )),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        side: const BorderSide(color: whiteColor)),
-                    onPressed: () {},
-                    child: "Logout".text.fontFamily(semibold).white.make())
-              ],
+            Padding(
+              padding: const EdgeInsets.only(right: 8, top: 8),
+              child: Align(
+                  alignment: Alignment.topRight,
+                  child: const Icon(
+                    Icons.edit,
+                    color: whiteColor,
+                  ).onTap(() {})),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    imgProfile,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ).box.roundedFull.clip(Clip.antiAlias).make(),
+                  5.heightBox,
+                  Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      "Dummy User".text.fontFamily(semibold).white.make(),
+                      "customer@example.com".text.white.make()
+                    ],
+                  )),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: whiteColor)),
+                      onPressed: () {},
+                      child: "Logout".text.fontFamily(semibold).white.make())
+                ],
+              ),
             ),
             20.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                DetailsCart(
-                  count: "16",
-                  title: "in your cart",
-                  width: context.screenWidth / 3.4,
-                ),
-                DetailsCart(
-                  count: "65",
-                  title: "in your wishlist",
-                  width: context.screenWidth / 3.4,
-                ),
-                DetailsCart(
-                  count: "305",
-                  title: "you ordered",
-                  width: context.screenWidth / 3.4,
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DetailsCart(
+                    count: "16",
+                    title: "in your cart",
+                    width: context.screenWidth / 3.4,
+                  ),
+                  DetailsCart(
+                    count: "65",
+                    title: "in your wishlist",
+                    width: context.screenWidth / 3.4,
+                  ),
+                  DetailsCart(
+                    count: "305",
+                    title: "you ordered",
+                    width: context.screenWidth / 3.4,
+                  ),
+                ],
+              ),
+            ),
+            ListView.separated(
+              shrinkWrap: true,
+              separatorBuilder: (context, index) {
+                return const Divider(
+                  color: lightGrey,
+                );
+              },
+              itemCount: profileButtonList.length,
+              itemBuilder: (BuildContext context, index) {
+                return ListTile(
+                  leading: Image.asset(
+                    profileButtonIcon[index],
+                    width: 22,
+                  ),
+                  title: profileButtonList[index]
+                      .text
+                      .fontFamily(semibold)
+                      .color(darkFontGrey)
+                      .make(),
+                );
+              },
             )
+                .box
+                .white
+                .rounded
+                .margin(const EdgeInsets.all(12))
+                .padding(
+                  const EdgeInsets.symmetric(horizontal: 16),
+                )
+                .shadowSm
+                .make()
+                .box
+                .color(redColor)
+                .make()
           ],
         ),
       )),
-    ));
+    );
   }
 }
