203ec42bdae2277a3330501e8e2f92dbb65db858
abdalla3yash <abdo3yash@gmail.com>
2023-01-19 03:56:53 +0200

    cart

--- lib/views/cart/cart_screen.dart
+++ lib/views/cart/cart_screen.dart
@@ -1,4 +1,6 @@
+import 'package:get/get.dart';
 import 'package:mart/consts/consts.dart';
+import 'package:mart/widget/widgets.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -24,6 +26,32 @@ class CartScreen extends StatelessWidget {
                   color: Colors.red,
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  "Total price"
+                      .text
+                      .fontFamily(semibold)
+                      .color(darkFontGrey)
+                      .make(),
+                  "40 \$".text.fontFamily(semibold).color(redColor).make(),
+                ],
+              )
+                  .box
+                  .padding(const EdgeInsets.all(12))
+                  .color(lightGolden)
+                  .roundedSM
+                  .make(),
+              SizedBox(
+                width: context.screenWidth - 60,
+                child: customButton(
+                    color: redColor,
+                    title: "Proceed To Shipping",
+                    textColor: whiteColor,
+                    onpress: () {
+                      // Get.to();
+                    }),
+              )
             ],
           )),
     );
